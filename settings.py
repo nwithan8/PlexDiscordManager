f69d939f0c8b99ab226f4880cf0788db80ab4016
Nate Harris <nwithan8@users.noreply.github.com>
2021-01-10 01:19:17 -0700

    Removed emojis from sample role names, was causing issues on IDLE

--- settings.py
+++ settings.py
@@ -19,20 +19,20 @@ ENABLE_BLACKLIST = True
 # Discord settings
 DISCORD_SERVER_ID = 'XXXXXXXX'
 DISCORD_ADMIN_ID = 'XXXXXXXXXXX'
-DISCORD_ADMIN_ROLE_NAME = 'Admin 👑'
+DISCORD_ADMIN_ROLE_NAME = 'Admin'
 
 
 # Subscriber settings
 INVITED_ROLE = "Invited" # Role given after someone is added to Plex
 AUTO_CHECK_SUBS = False
-SUB_ROLES = ["Monthly Subscriber 🕒", "Yearly Subscriber 📅", "Winner 🏆", "Lifetime Subscriber ⛰️", "Bot"]  # Users with any of these roles is exempt from removal
+SUB_ROLES = ["Monthly Subscriber", "Yearly Subscriber", "Winner", "Lifetime Subscriber", "Bot"]  # Users with any of these roles is exempt from removal
 EXEMPT_SUBS = [DISCORD_ADMIN_ID]  # Discord IDs for users exempt from subscriber checks/deletion, separated by commas
 SUB_CHECK_TIME = 7  # days
 CURRENTLY_PLAYING_ROLE_NAME = 'Watching'
 
 
 # Trial settings
-TRIAL_ROLE_NAME = "Trial ⏰"  # Role given to a trial user
+TRIAL_ROLE_NAME = "Trial"  # Role given to a trial user
 TRIAL_LENGTH = 24 * 60 * 60  # (seconds) How long a trial lasts
 TRIAL_CHECK_FREQUENCY = 15  # (minutes) How often the bot checks for trial expirations
 
